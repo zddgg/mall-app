80be37f99c73599344dea8287c743eb4458e30e4
liuwenliang <liuwenliang001@163.com>
2023-03-03 23:42:14 +0800

    feat: 样式调整

---
+++ src/pages/cart/components/preferenceCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <view style="margin-left: 40px; display: flex; justify-content: space-between">
+    <view style="display: flex;">
+      <nut-tag color="#f93e1e" size="12px">
+        跨品类满折
+      </nut-tag>
+      <view style="font-size: 14px">满2类总价9.8折，还差1类</view>
+    </view>
+    <view>
+      去凑单
+      <icon-font name="right" size="12px"></icon-font>
+    </view>
+  </view>
+</template>
+
+<script lang="ts" setup>
+import {IconFont} from "@nutui/icons-vue-taro";
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/cart/components/storeCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <view style="display: flex; justify-content: space-between">
+    <view style="display: flex">
+      <view style="width: 40px">
+        <nut-checkbox v-model="checkbox1">
+        </nut-checkbox>
+      </view>
+      <view style="display: flex">
+        <view>JD 华为自营旗舰店</view>
+        <view>
+          <icon-font name="right" size="12px"></icon-font>
+        </view>
+      </view>
+    </view>
+    <!--  右侧标签  -->
+    <view style="display: flex">
+      <view>
+        <icon-font name="issue" size="20px"></icon-font>
+      </view>
+      <view style="font-size: 20px">以免运费</view>
+    </view>
+  </view>
+</template>
+
+<script lang="ts" setup>
+import {ref} from "vue";
+import {IconFont} from "@nutui/icons-vue-taro";
+
+const checkbox1 = ref(false);
+</script>
+
+<style scoped>
+
+</style>
